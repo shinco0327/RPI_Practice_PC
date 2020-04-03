9caaef84770cf897bbf52fabe9eb0b489a342810
shinco0327 <B10707049@gapps.ntust.edu.tw>
2020-04-03 22:32:46 +0800

    UI update

--- Mongo_randNum.py
+++ Mongo_randNum.py
@@ -11,11 +11,12 @@ matplotlib.use('TkAgg') #Use tkAgg
 
 y_plot = [0]*10
 x_plot = [0]*10
+graph_update_time = 0
 
 myclient = pymongo.MongoClient('mongodb://192.168.0.13:27017')
 print(myclient.list_database_names())
 mydb = myclient["test_database"]
-mycol = mydb["Running_time"]
+mycol = mydb["VR_test"]
 
 
 
@@ -23,13 +24,14 @@ mycol = mydb["Running_time"]
 Window1 = tk.Tk()
 Window1.title("Analysis")
 Window1.geometry('640x480')
-title1 = tk.Label(Window1, text="Chart of Number")
+title1 = tk.Label(Window1, text="Current time:")
 title1.grid(row=0, column=0, sticky='w')
 
 
 fig = plt.figure(figsize=[6, 6])
 x_format = matplotlib.dates.DateFormatter('%m-%d %H:%M')
 plt.autoscale(enable=True, axis='x', tight=False)
+plt.title("Voltage of VR")
 plt.xlabel("Time")
 plt.ylabel("Voltage(V)")
 plt.ylim(0, 3.5)
@@ -53,7 +55,20 @@ canvas.mpl_connect("key_press_event", on_key_press)
  
 
 def update_plot():
-    cursor = mycol.find({})
+    global graph_update_time
+    global title1
+    title1_str = "Current time: "+ time.asctime(time.localtime(time.time()))
+    title1.configure(text=title1_str)
+    if time.time()-graph_update_time >= 60:
+        plt.clf()
+        x_format = matplotlib.dates.DateFormatter('%m-%d %H:%M')
+        plt.autoscale(enable=True, axis='x', tight=False)
+        plt.title("Voltage of VR")
+        plt.xlabel("Time")
+        plt.ylabel("Voltage(V)")
+        plt.ylim(0, 3.5)
+        graph_update_time = time.time()
+    cursor = mycol.find(sort=[("_id", pymongo.DESCENDING)]).limit(10)
     count = 0
     dates = [0]*10
     for document in cursor:
